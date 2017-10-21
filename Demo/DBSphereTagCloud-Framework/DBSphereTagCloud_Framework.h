aed6230d2ea1cbd99e1c6c3826eb7155f9e757a2
Aaron Lee <aaron@onefatgiraffe.com>
2017-10-22 00:20:54 +0900

    Carthage support

---
+++ Demo/DBSphereTagCloud-Framework/DBSphereTagCloud_Framework.h
@@ -0,0 +1,19 @@
+//
+//  DBSphereTagCloud_Framework.h
+//  DBSphereTagCloud-Framework
+//
+//  Created by Aaron Lee on 2017/10/22.
+//  Copyright © 2017 Xinbao Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DBSphereTagCloud_Framework.
+FOUNDATION_EXPORT double DBSphereTagCloud_FrameworkVersionNumber;
+
+//! Project version string for DBSphereTagCloud_Framework.
+FOUNDATION_EXPORT const unsigned char DBSphereTagCloud_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DBSphereTagCloud_Framework/PublicHeader.h>
+
+
